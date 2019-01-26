278cd52e98fbc3734bd03a93b49ac41c162cb976
glen <glen_sale@yahoo.com>
2019-01-25 16:13:05 -0800

    it will add services for api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NBA } from './models/nba.model';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  nba$: NBA[];
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    return this.dataService.getUsers()
+      .subscribe(data => this.nba$ = data);
+  }
 }
 
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NBA } from './models/nba.model';
 
 @Injectable()
 export class DataService {
+  apiUrl = 'http://developers.stattleship.com';
+  constructor(private _http: HttpClient) { }
 
-  constructor() { }
-
+  getUsers() {
+    return this._http.get<NBA[]>(this.apiUrl);
+  }
 }
